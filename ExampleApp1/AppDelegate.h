06d523feda225654db61f9eba47eb41f4e60a5cf
Spencer Atkin <spenceratkin@SocialRadar-Intern-MacBook-Pro.local>
2015-06-23 10:15:05 -0400

    Initial Commit

---
+++ ExampleApp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleApp1
+//
+//  Created by Spencer Atkin on 6/23/15.
+//  Copyright (c) 2015 Spencer Atkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
